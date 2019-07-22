af00497bab2bf57c9a603dea810236b19e9cf3d3
Iriel <irieljm@gmail.com>
2019-07-23 01:49:26 +0200

    page-containers + start of recipe page

--- app/views.py
+++ app/views.py
@@ -3,6 +3,12 @@ from app import app
 import requests, json
 
 
+@app.route('/recipe/<idMeal>')
+def get_recipe(idMeal):
+	url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal
+	cake = requests.get(url).json().get("meals", [{}])[0]
+	return render_template("recipe.html", cake=cake)
+
 @app.route('/')
 def index():
 	url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert"
